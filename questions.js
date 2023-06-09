dd36dd331129d58ec34d8c1d0d01e055ceae9a8a
Khujaniyazova <Khujaniyazova00@gmail.com>
2023-06-08 23:28:51 -0700

    added qts

--- questions.js
+++ questions.js
@@ -1,68 +1,175 @@
 const questions = [
   {
-    question: "What is the capital of France?",
-    options: ["A) Paris", "B) London", "C) Madrid", "D) Rome"],
+    question:
+      "Bu shaxmat donasi sabab o'rta asrlarda arab shohlari bu o'yinni o'ynashni istashmagan.",
+    options: ["A) Ot", "B) Fil", "C) Shoh", "D) Piyoda"],
     answer: "A",
   },
   {
-    question: "What is the largest planet in our solar system?",
-    options: ["A) Jupiter", "B) Saturn", "C) Uranus", "D) Neptune"],
+    question:
+      "Bu harf deyarli dunyoning barchа alfavitlarida 1 xil yozilishi bilan rekord o'rnatgan. Bu qaysi harf?",
+    options: ["A) O", "B) A", "C) C", "D) K"],
     answer: "A",
   },
   {
-    question: "What is the tallest mountain in the world?",
-    options: ["A) Mount Everest", "B) K2", "C) Kangchenjunga", "D) Lhotse"],
-    answer: "A",
-  },
-  {
-    question: "What is the smallest country in the world?",
-    options: [
-      "A) Monaco",
-      "B) Vatican City",
-      "C) San Marino",
-      "D) Liechtenstein",
-    ],
+    question:
+      "Avstraliyada yashovchi mutlaqo suv ichmaydigan sut emizuvchi hayvon?",
+    options: ["A) Kenguru", "B) Koala", "C) Tuyaqush", "D) Tuya"],
     answer: "B",
   },
   {
-    question: "What is the capital of Australia?",
-    options: ["A) Sydney", "B) Melbourne", "C) Canberra", "D) Brisbane"],
+    question:
+      "Xaritani kuzatganimizda « Dinozavr » shakldagi mamlakat nomini bilasizmi?",
+    options: ["A) Yaponiya", "B) Avstraliya", "C) Yangi Gvineya", "D) Chili"],
     answer: "C",
   },
   {
-    question: "What is the largest ocean in the world?",
+    question: "Bilasizmi? Koreya so`zining ma`nosi nima?",
     options: [
-      "A) Atlantic Ocean",
-      "B) Indian Ocean",
-      "C) Pacific Ocean",
-      "D) Arctic Ocean",
+      "A) Tonggi tarovat",
+      "B) Tarovvatli",
+      "C) Sharq tarovvati",
+      "D) Go'zal shahar",
     ],
-    answer: "C",
+    answer: "A",
   },
   {
-    question: "What is the highest waterfall in the world?",
-    options: [
-      "A) Niagara Falls",
-      "B) Angel Falls",
-      "C) Victoria Falls",
-      "D) Iguazu Falls",
-    ],
+    question:
+      "Qaysi davlat jahonda pochta xizmati tekin bo`lgan yagona davlat hisoblanadi?",
+    options: ["A) Argentina", "B) Andorra", "C) Gretsiya", "D) Vetnam"],
     answer: "B",
   },
   {
-    question: "What is the most populous country in the world?",
-    options: ["A) India", "B) United States", "C) China", "D) Russia"],
+    question: "Qaysi mamlakat hududida 17 ta harakatdagi vulqon joylashgan? ",
+    options: ["A) Braziliya", "B) Koreya", "C) Yaponiya", "D) Indoneziya"],
     answer: "C",
   },
   {
-    question: "What is the smallest continent in the world?",
-    options: ["A) Europe", "B) Asia", "C) Africa", "D) Australia"],
+    question: "Qaysi davlat poytaxti dunyodagi eng sovuq poytaxt hisoblanadi?",
+    options: ["A) Sibir", "B) Buenos-Ayres", "C) Mongoliya", "D) Moskva"],
+    answer: "C",
+  },
+  {
+    question:
+      "Qaysi sayyora Quyosh tizimidagi eng tezkor sayyora hisoblanadi? ",
+    options: ["A) Saturn", "B) Neptun", "C) Uran", "D) Yupiter"],
     answer: "D",
   },
   {
-    question: "What is the currency of Japan?",
-    options: ["A) Yen", "B) Dollar", "C) Euro", "D) Pound"],
+    question:
+      "Qaysi davlat temir yo`llarining zichligi bo`yichа Yevropada 1- o`rinda turadi?",
+    options: [
+      "A) Belgiya",
+      "B) Belorusiya",
+      "C) Buyuk britaniya",
+      "D) Fransiya",
+    ],
     answer: "A",
+  },
+  {
+    question:
+      "Xaritada jirafa shaklidagi orollarda joylashgan mamlakat nomini bilasizmi?",
+    options: [
+      "A) Finlandiya",
+      "B) Norvegiya",
+      "C) Yaponiya",
+      "D) Yangi Zelandiya",
+    ],
+    answer: "C",
+  },
+  {
+    question: "Qaysi mamlakat tarkibida 17 ming 508 ta orol mavjud?",
+    options: ["A) Norvegiya", "B) Yaponiya", "C) Grelandiya", "D) Indoneziya"],
+    answer: "D",
+  },
+  {
+    question: "Bilasizmi, qaysi hayvon umrining 70 foizini uxlab o`tkazadi?",
+    options: ["A) Mushuk", "B) It", "C) Ayiq", "D) Sichqon"],
+    answer: "A",
+  },
+  {
+    question:
+      "Qaysi hayvon ( hashorat) ning zahari kattalarinikidan kuchliroq bo`ladi? ",
+    options: ["A) Ilon", "B) Chayon", "C) Pashsha", "D) Ari"],
+    answer: "B",
+  },
+  {
+    question:
+      "Yer yuzida qaysi hayvon yuragi bir daqiqada atigi 9 marta uradi? ",
+    options: ["A) Ayiq", "B) Yalqov", "C) Kit", "D) Akula"],
+    answer: "C",
+  },
+  {
+    question:
+      "Birlashgan Millatlar Tashkiloti Universiteti qayerda joylashgan?",
+    options: ["A) Tokio", "B) AQSH", "C) Rossiya", "D) Xitoy"],
+    answer: "A",
+  },
+  {
+    question:
+      "Qadimgi Rimliklarning fikrichа, eng shafqatsiz xo'jayinlar- ular. Savol: ular kimlar? ",
+    options: ["A) Hukmdorlar", "B) Boylar", "C) Sobiq qullar", "D) Feodallar"],
+    answer: "C",
+  },
+  {
+    question: "Hazil savol: Eng ko'p haqorat eshitadigan kishilar kimlar?",
+    options: ["A) Futboi haqida", "B) Sudiya", "C) Oshpaz", "D) Kotib"],
+    answer: "A",
+  },
+  {
+    question: "",
+    options: ["A) ", "B) ", "C) ", "D) "],
+    answer: "",
+  },
+  {
+    question: "",
+    options: ["A) ", "B) ", "C) ", "D) "],
+    answer: "",
+  },
+  {
+    question: "",
+    options: ["A) ", "B) ", "C) ", "D) "],
+    answer: "",
+  },
+  {
+    question: "",
+    options: ["A) ", "B) ", "C) ", "D) "],
+    answer: "",
+  },
+  {
+    question: "",
+    options: ["A) ", "B) ", "C) ", "D) "],
+    answer: "",
+  },
+  {
+    question: "",
+    options: ["A) ", "B) ", "C) ", "D) "],
+    answer: "",
+  },
+  {
+    question: "",
+    options: ["A) ", "B) ", "C) ", "D) "],
+    answer: "",
+  },
+  {
+    question: "",
+    options: ["A) ", "B) ", "C) ", "D) "],
+    answer: "",
+  },
+  {
+    question: "",
+    options: ["A) ", "B) ", "C) ", "D) "],
+    answer: "",
+  },
+  {
+    question: "",
+    options: ["A) ", "B) ", "C) ", "D) "],
+    answer: "",
+  },
+  {
+    question: "",
+    options: ["A) ", "B) ", "C) ", "D) "],
+    answer: "",
   },
 ];
 
